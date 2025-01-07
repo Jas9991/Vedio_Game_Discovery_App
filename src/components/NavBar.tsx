652dc6c64788bc99196306e41e161cd944e392cd
Jas9991 <jaspreetsinghrathi99@gmail.com>
2025-01-07 14:49:05 +0530

    Give my Logo and Message

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,24 @@
-import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/jas13.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <div>
+        <HStack>
+          <Image
+            borderRadius={10}
+            overflow="hidden"
+            src={logo}
+            boxSize="60px"
+          />
+          <Text color={"grey"}>
+            Vedio Games Discovery Web App , just like Airbnb is for finding
+            rooms , this App is for finding games by Genres or by Platforms.{" "}
+          </Text>
+        </HStack>
+      </div>
       <ColorModeSwitch />
     </HStack>
   );
